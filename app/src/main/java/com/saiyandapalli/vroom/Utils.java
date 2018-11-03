1661a4450b52a9545aeddb996459ea567ba3945d
Sai Yandapalli <saiyandapalli07@berkeley.edu>
2018-11-03 05:06:16 -0700

    abhi pulls after firebase

---
+++ app/src/main/java/com/saiyandapalli/vroom/Utils.java
@@ -0,0 +1,63 @@
+package com.saiyandapalli.vroom;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Utils{
+    public static FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    public static void attemptLogin(final Context context, String email, String password) {
+        if (!email.equals("") && !password.equals("")) {
+            MainActivity.mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener((Activity) context, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d("Log in", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                            if (!task.isSuccessful()) {
+                                Log.w("Aww", "signInWithEmail:failed", task.getException());
+                                Toast.makeText(context, "Sign in failed!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                context.startActivity(new Intent(context, MapActivity.class));
+                            }
+                        }
+                    });
+        }
+    }
+
+    public static void attemptSignup(final Context context, String email, String password) {
+
+        if (!email.equals("") && !password.equals("")) {
+            MainActivity.mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener((Activity)context, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d("Yay", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                            if (!task.isSuccessful()) {
+                                Log.d("Yay", task.getException().getMessage());
+                                Toast.makeText(context, "Failed Signup", Toast.LENGTH_SHORT).show();
+                            } else {
+                                context.startActivity(new Intent(context, MapActivity.class));
+                            }
+                        }
+                    });
+        }
+    }
+}
